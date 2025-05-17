2782de0d01b1f147f1858d3fc98b03048f589afe
cha0s86 <pekkakukonlehto@outlook.com>
2025-05-17 16:33:59 +0300

    Christmas tree!

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    std::cout << "Hello, world of christmas!" << "\n" << "\n";
+
+    std::cout << "    *     " << "\n";
+    std::cout << "   /|\\   " << "\n";
+    std::cout << "  / | \\  " << "\n";
+    std::cout << " /__|__\\ " << "\n";
+    std::cout << "    |     " << "\n";
+
+    return 0;
+}
